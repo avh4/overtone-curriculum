532371cbb1aa86620ed1ab2e933990391c99d7d4
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-09-13 19:01:07 -0700

    Define the metronome inside of chord-progression-beat)

--- 01_play_music.clj
+++ 01_play_music.clj
@@ -30,18 +30,20 @@
   (play inst (chord root chord-name)))
 
 
-(defn chord-progression-beat [m]
-  (at (m  0) (play-chord pad :C4 :major))
-  (at (m  4) (play-chord pad :G3 :major))
-  (at (m  8) (play-chord pad :A3 :minor))
-  (at (m 12) (play-chord pad :F3 :major))
+(defn chord-progression-beat []
+  (let [m (metronome 240)]
+    (at (m  0) (play-chord pad :C4 :major))
+    (at (m  4) (play-chord pad :G3 :major))
+    (at (m  8) (play-chord pad :A3 :minor))
+    (at (m 12) (play-chord pad :F3 :major))
   
-  (doseq [i (range 0 16 3)]
-    (at (m i) (drum))
+    (doseq [i (range 0 16 3)]
+      (at (m i) (drum))
+    )
+  
+    (apply-at (m 16) #(chord-progression-beat))
   )
-  
-  (apply-at (m 16) #(chord-progression-beat (metronome 240)))
 )
-(chord-progression-beat (metronome 240))
+(chord-progression-beat)
 
 ; (stop)
