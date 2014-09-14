3801af8a681dcc7e31b32f826df0e8d3d6b98011
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-09-13 18:02:28 -0700

    Simplify calling of play-chord

--- 01_play_music.clj
+++ 01_play_music.clj
@@ -23,16 +23,16 @@
 ; (pad 440)
 ; (drum)
 
-(defn play-chord [inst a-chord]
-  (doseq [note a-chord] (inst (midi->hz note))))
+(defn play-chord [inst root chord-name]
+  (doseq [note (chord root chord-name)] (inst (midi->hz note))))
 
 
 (def metro (metronome 240))
 (defn chord-progression-beat [m beat-num]
-  (at (m (+ 0 beat-num)) (play-chord pad (chord :C4 :major)))
-  (at (m (+ 4 beat-num)) (play-chord pad (chord :G3 :major)))
-  (at (m (+ 8 beat-num)) (play-chord pad (chord :A3 :minor)))
-  (at (m (+ 12 beat-num)) (play-chord pad (chord :F3 :major)))
+  (at (m (+ 0 beat-num)) (play-chord pad :C4 :major))
+  (at (m (+ 4 beat-num)) (play-chord pad :G3 :major))
+  (at (m (+ 8 beat-num)) (play-chord pad :A3 :minor))
+  (at (m (+ 12 beat-num)) (play-chord pad :F3 :major))
   
   (for [i (range 0 16 3)]
     (at (m (+ i beat-num)) (drum 440))
